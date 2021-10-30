dbeb5028dc071543676f154b8494b7377a0cd898
Jean-Pierre Perroud <31518494+jperroud@users.noreply.github.com>
2021-10-31 00:19:13 +0200

    Commit initial

---
+++ rnd.f95
@@ -0,0 +1,43 @@
+!------------------------------------------------------------
+! Génération d'une table de nombres aléatoires
+!
+! Programmation au format libre
+!
+! Jean-Pierre Perroud,  octobre 2010
+!
+!	mise a jour :   janvier 2012
+!
+!------------------------------------------------------------
+
+program rnd 
+
+
+! Fonctions  internes
+      	intrinsic random_seed, random_number 
+
+! Définition des variables
+      	integer 	:: i 
+      	real 		:: valeur, valeur2, valeur3 
+      	character	:: esp = ' '
+	
+
+! Initialisation du générateur aléatoire 
+	call random_seed 
+
+	write (*,*) ' '
+	write (*,*) 'Liste de nombres aléatoires '
+	write (*,*) '----------------------------'	
+	write (*,*) ' '
+
+      	do i = 1,35 
+      		call random_number(valeur)
+		valeur2 = 10  * valeur
+		valeur3 = 100 * valeur 
+      		write (*,'(a3,f8.6,f8.4,f8.2)') esp, valeur, valeur2, valeur3
+	enddo 
+	
+	write (*,*) ' '
+  
+end program rnd
+
+!-----------------------------------------------------------------
